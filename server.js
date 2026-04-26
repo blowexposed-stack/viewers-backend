182904943b6c891e2db7adc0a173250d6312830c
blowexposed-stack <blowexposed@gmail.com>
2026-04-26 17:26:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 
 const http      = require('http');
+// ADICIONE ESTA LINHA:
+const { Server } = require('socket.io'); 
 const app       = require('./app');
 const connectDB = require('./database');
 const logger    = require('./logger');
@@ -12,9 +14,24 @@ async function startServer() {
   try {
     await connectDB();
     const server = http.createServer(app);
+
+    // ADICIONE ESTE BLOCO:
+    const io = new Server(server, {
+      cors: {
+        origin: "https://comunidadeviewers.vercel.app", // Isso libera o acesso da Vercel
+        methods: ["GET", "POST"]
+      }
+    });
+
+    // Isso faz o socket funcionar de verdade
+    io.on('connection', (socket) => {
+      console.log('Usuário conectado:', socket.id);
+    });
+
     server.listen(PORT, () => {
       logger.info(`Servidor rodando na porta ${PORT} [${process.env.NODE_ENV}]`);
     });
+    
     process.on('SIGTERM', () => server.close(() => process.exit(0)));
     process.on('SIGINT',  () => server.close(() => process.exit(0)));
   } catch (err) {
