2b118fe50cf7390854143fd4d90425e68f6f68aa
blowexposed-stack <blowexposed@gmail.com>
2026-04-26 19:34:54 -0300

    Update streamer.routes.js

--- streamer.routes.js
+++ streamer.routes.js
@@ -1,13 +1,13 @@
 'use strict';
+// streamer.routes.js (Temporário para teste)
 const express = require('express');
 const router = express.Router();
 const streamerController = require('./streamer.controller');
-const { protect } = require('./auth.middleware'); // Se você usar proteção
+// const { protect } = require('./auth.middleware'); <--- Comente essa linha
 
-// Verifique se os nomes depois do ponto (.) são IGUAIS aos do controller
 router.get('/', streamerController.getLiveStreamers); 
-router.get('/me', protect, streamerController.getMyStream);
-router.patch('/me/go-live', protect, streamerController.goLive);
-router.patch('/me/go-offline', protect, streamerController.goOffline);
+router.get('/me', streamerController.getMyStream); // Retire o 'protect' daqui
+router.patch('/me/go-live', streamerController.goLive); // Retire o 'protect' daqui
+router.patch('/me/go-offline', streamerController.goOffline); // Retire o 'protect' daqui
 
 module.exports = router;
